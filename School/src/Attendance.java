dae46fc01c3f91aa8644b20c1ae64e17de98d7e2
Naseer <nasnaseer6@gmail.com>
2023-06-15 12:26:13 +0530

    Student ATTENDANCE CREATED

---
+++ School/src/Attendance.java
@@ -0,0 +1,30 @@
+
+public class Attendance {
+
+	
+	
+	public static void main(String[] args) {
+		
+		Child c=new Child();
+		c.display();
+		c.displays();
+		
+
+		// TODO Auto-generated method stu
+	}
+
+}
+class Parent
+{
+public void displays()
+{
+	System.out.println("Parent method");
+}
+}
+class Child extends Parent
+{
+	public void display()
+	{
+		System.out.println("Child method");
+	}
+}
